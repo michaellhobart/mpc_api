7bb7b4d0042143ae71a4b82359a9dfc4623c1a05
Mike Hobart <mikehobart@LOY-TECH-4762L.local>
2019-09-18 13:59:40 -0400

    Added updateclient PUT endpoint
    Added an endpoint for updating clients that are already in the database.

--- server.js
+++ server.js
@@ -22,6 +22,23 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
 })
 
+app.put('/updateclient/:id', (req, res) => {
+  const clientId = req.params.id
+  const updateClient = req.body
+
+  knex('mpc_clients_test')
+    .returning(['id', 'client_name', 'email', 'phone', 'contact'])
+    .where('id', '=', clientId)
+    .update({
+      client_name: updateClient.client_name,
+      email: updateClient.email,
+      phone: updateClient.phone,
+      contact: updateClient.contact
+    })
+    .then(data => res.status(200).json(data))
+    .catch(e => res.status(500).send(`Error: ${e}`))
+})
+
 app.listen(port, () => {
   console.log(`Server listening on ${port}`)
 })
