3f8729b5b8955f4338d837b0a20db23b496572ae
Mike Hobart <mikehobart@LOY-TECH-4762L.local>
2019-09-18 12:28:43 -0400

    Added linter to file
    Added Standard JS linter standards to the app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser')
 
-app.use(body_parser.json());
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 4000
 
@@ -16,33 +16,32 @@ const knex = require('knex')({
     database: process.env.DB,
     password: process.env.PASSWORD
   }
-});
+})
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+})
 
-app.get("/", (req, res) => {
-  res.json({message: "Hello World!"});
-});
-
-app.post("/newclient", (req, res) => {
-  const newClient = req.body;
-  console.log(newClient);
+app.post('/newclient', (req, res) => {
+  const newClient = req.body
+  console.log(newClient)
 
   knex('mpc_clients_test')
-  .returning(['id', 'client_name'])
-  .insert({
-    client_name: newClient.client_name,
-    email: newClient.email,
-    phone: newClient.phone,
-    contact: newClient.contact
-  })
-  .then( data => {
-    res.status(200).json(data)
-  })
-  .catch(e => {
-    res.status(500).send(`Error: ${e}`)
-  })
+    .returning(['id', 'client_name'])
+    .insert({
+      client_name: newClient.client_name,
+      email: newClient.email,
+      phone: newClient.phone,
+      contact: newClient.contact
+    })
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(e => {
+      res.status(500).send(`Error: ${e}`)
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+  console.log(`Server listening on ${port}`)
 })
